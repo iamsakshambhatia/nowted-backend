a7950575dc88a20e6260be657395c057c59445ce
Saksham Bhatia <iamsakshambhatia@gmail.com>
2025-06-07 18:46:08 +0530

    fix database connection

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -5,7 +5,7 @@ const poolConfig = {
   user: process.env.DB_USER ?? "",
   port: Number(process.env.DB_PORT) ?? "",
   password: process.env.DB_PASSWORD ?? "",
-  name: process.env.DB_NAME ?? "",
+  database: process.env.DB_NAME ?? "",
 };
 
 export const pool = new Pool(poolConfig);
@@ -13,7 +13,7 @@ export const pool = new Pool(poolConfig);
 async function dbConnection() {
   try {
     const client = await pool.connect();
-    console.log("db connected successfully");
+    if (client) console.log("db connected successfully");
   } catch (error) {
     console.error("error in connecting to db");
   }
